d4aa97cb8d42de38d631be415208f1234c275464
Py Zhao <zhaopy4721@outlook.com>
2019-03-14 14:53:20 +0800

    Add Thread support.

--- app/src/main/java/top/contrail/py/androidurl/GetInfo.java
+++ app/src/main/java/top/contrail/py/androidurl/GetInfo.java
@@ -1,5 +1,7 @@
 package top.contrail.py.androidurl;
 
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -22,6 +24,8 @@ class GetInfo {
     private String cookies;
     private String jsonData;
 
+    private Handler handler;
+
     GetInfo(String aid_) {
         this.aid = aid_;
         this.result_json = null;
@@ -29,46 +33,67 @@ class GetInfo {
 
     }
 
-    void set_parameter(String url, String cookie) {
+    void set_parameter(String url, String cookie, Handler handler_in) {
 
         this.base_url = url;
         this.cookies = cookie;
-
+        this.handler = handler_in;
 
     }
 
     void send_request() {
 
         final String put_aid = this.aid;
+        final String url = this.base_url;
+        final String result_str;
 
-        try {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(this.base_url)
-                    .build();
-            Response responses = null;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-            try {
-                responses = client.newCall(request).execute();
-            } catch (IOException e) {
-                Log.e(TAG, e.toString());
-                e.printStackTrace();
-            }
-            this.jsonData = responses.body().string();
-            JSONObject Jobject = new JSONObject(this.jsonData);
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .build();
+                    Response responses = null;
+
+                    try {
+                        responses = client.newCall(request).execute();
+                    } catch (IOException e) {
+                        Log.e(TAG, e.toString());
+                        e.printStackTrace();
+                    }
+
+                    try {
+//                        this.jsonData = responses.body().string();
+//                    result_str = responses.body().string();
+                        Log.d(TAG, responses.body().string());
+
+                    }catch (NullPointerException e){
+                        Log.e(TAG, e.toString());
+                        e.printStackTrace();
+                    }
+
+
+//                    JSONObject Jobject = new JSONObject(this.jsonData);
 //            JSONArray Jarray = Jobject.getJSONArray("employees");
 //
 //            for (int i = 0; i < Jarray.length(); i++) {
 //                JSONObject object     = Jarray.getJSONObject(i);
 //            }
 
-            this.result_json = Jobject.toString();
+//                    this.result_json = Jobject.toString();
 
-        }catch (JSONException e){
-            Log.e(TAG, e.toString());
-        }catch (IOException e) {
-            Log.e(TAG, e.toString());
-        }
+//                }catch (JSONException e){
+//                    Log.e(TAG, e.toString());
+                }catch (IOException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        }).start();
+
+
     }
 
     void analyse_result() {
--- app/src/main/java/top/contrail/py/androidurl/MainActivity.java
+++ app/src/main/java/top/contrail/py/androidurl/MainActivity.java
@@ -1,8 +1,12 @@
 package top.contrail.py.androidurl;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +17,27 @@ public class MainActivity extends AppCompatActivity {
     EditText aid=null;
     Button get=null;
     TextView content=null;
+
+    private static final String TAG = "MainActivity";
+
+//    Handler handler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            Werther werther= (Werther) msg.obj;
+//            switch(msg.what){
+//                case 1:
+//                    tv_werther_conten.setText("时间：" + String.valueOf(werther.getHeWeather6().get(0).getUpdate().getLoc()) +
+//                            "、地点：" + werther.getHeWeather6().get(0).getBasic().getLocation() +
+//                            "、天气：" + werther.getHeWeather6().get(0).getNow().getCond_txt() +
+//                            "、风向：" + werther.getHeWeather6().get(0).getNow().getWind_dir() +
+//                            "、气温：" + werther.getHeWeather6().get(0).getNow().getTmp());
+//                    break;
+//            }
+//            Log.d(TAG, "onResponse: " + String.valueOf(werther.getHeWeather6().get(0).getUpdate().getLoc()));
+//
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +54,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override public void onClick(View v) {
 
-//                String base_url = getResources().getString(R.string.URL_UP_ALBUM);
-                String base_url = "https://www.baidu.com";
+                String base_url = getResources().getString(R.string.URL_UP_ALBUM);
                 String aid_number = aid.getText().toString();
                 String cookies = getResources().getString(R.string.COOKIES);
 
                 GetInfo task = new GetInfo(aid_number);
-                task.set_parameter(base_url, cookies);
+//                task.set_parameter(base_url, cookies, handler);
                 task.send_request();
                 task.analyse_result();
-
                 String info = task.get_response();
-
                 content.setText(info);
+
+//                MyAsyncTask task = new MyAsyncTask();
+//                task.execute(base_url, aid_number, cookies);
 
             }
 
         });
 
     }
+
 }
